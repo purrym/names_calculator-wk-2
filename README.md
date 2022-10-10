2f6623f8f9b32b4816d3d821c0264f7af0a16395
purrym <puritymunene2014@gmail.com>
2022-10-10 22:52:58 +0300

    updated styling and logic

--- README.md
+++ README.md
@@ -1,29 +1,33 @@
-# names_calculator-wk-2
-The project contains a well-documented README file.
-Project name.
--This project features a generator that derives an Akan names with regard to the day of the week that a user was born and their gender.
-Project description.
-In many parts of Ghana, and elsewhere in West Africa, a person’s name is determined by the day of the week on which she/he was born.
-This name generator takes a user's birthday(year, month and date) and gender information, determines the day of the week on which they were born and derives an Akan name depending on their gender.
+### **Akan Name Generator** by purity munene
+_This project features a generator that derives an Akan names with regard to the day of the week that a user was born and their gender._
 
 
+#### **Description**
+_In many parts of Ghana, and elsewhere in West Africa, a person’s name is determined by the day of the week on which she/he was born._
+<br>
+_This name generator takes a user's birthday(year, month and date) and gender information, determines the day of the week on which they were born and derives an Akan name depending on their gender._
 
-Author(s) information.
-Setup instructions - includes any scripts that need to be run if necessary.
-BDD.
-Write code to handle
-Wrong input entered
-No data entered
-Correct data entered
-Sample input
-1986 8 16 Female
-Expected Output
-Correct Akan name to match day of the week. Example Ama - Female born on a Saturda
-Technologies used.
-HTML
-CSS
-Bootstrap
-Javascript
-Contact information.
+#### **Languages Used**
+HTML <br>
+CSS <br>
+Bootstrap 
+Javascript 
 
-License and Copyright information.
+#### **BDD**
+*Write code to handle* <br>
+Wrong input entered <br>
+No data entered <br>
+Correct data entered <br>
+*Sample input* <br>
+1986    8   16  Female <br>
+*Expected Output* <br>
+Correct Akan name to match day of the week. Example Ama - Female born on a Saturday
+
+#### **Contact**
+If you need any assistance on any issues or have questions, ideas or concerns. Kindly contact me through email: wanjira.purity@gmail.com to make a contribution to the code or for any assistance on the app/project.
+>Live Link: 
+
+#### **Copyright & License**
+The License used is GPL_
+
+ Copyright (c) 2022 **Purity Munene**
--- js/main.js
+++ js/main.js
@@ -1,219 +1,74 @@
-var CC, YY, MM, DD, d, dayValue;
+function AkanNames() {
+  var daysOfWeek = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+  ];
+  var maleAkanNames = [
+      "Kwasi",
+      "Kwadwo",
+      "Kwabena",
+      "Kwaku",
+      "Yaw",
+      "Kofi",
+      "Kwame",
+  ];
+  var femaleAkanNames = [
+      "Akosua",
+      "Adwoa",
+      "Abenaa",
+      "Akua",
+      " Yaa",
+      "Afua",
+      "Ama",
+  ];
 
-const dayWeekNames = ["Sunday","Monday","Tuesday","Wednesday", "Thursday", "Friday","Saturday" ];
+  var dateInput = prompt("Enter date of birth YYYY-MM-DD");
 
-const maleAkanNames = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw", "Kofi","Kwame"];
+  var birthDates = new Date(dateInput);
+  var dateYenyewe = birthDates.getDay();
+  var birthMonth = birthDates.getMonth();
+  var birthYear = birthDates.getFullYear();
 
-const femaleAkanNames = ["Akosua","Adwoa","Abenaa","Akua"," Yaa","Afua","Ama"];
 
- 
+  var genderFemale = femaleAkanNames[dateYenyewe];
+  var genderMale = maleAkanNames[dateYenyewe];
 
-function validate() {
+  if (
+      dateYenyewe >= 0 &&
+      dateYenyewe <= 6 &&
+      birthMonth <= 11 &&
+      birthYear >= 1000
+  ) {
+      //logic
+      var gender = prompt("Enter your gender M/F: ").toUpperCase(); //changes into upper case
 
-  var genders = document.getElementsByName("gender");
+      if (
+          gender === "M" ||
+          gender === "F" ||
+          gender === "MALE" ||
+          gender === "FEMALE"
+      ) {
+          if (gender === "M") {
 
-  if( document.myForm.year.value == "" || document.myForm.year.value.length !=4 || document.myForm.year.value >2100 || document.myForm.year.value <=1900) {
+              document.getElementById('myFeedback').innerHTML = 'Your Akan name is' + genderMale;
 
-     alert( "Please provide a valid year of birth! eg 2019" );
 
-     document.myForm.year.focus() ;
+              // alert("your Akan name is" + genderMale);
+          } else {
 
-     return false;
-
+              document.getElementById('myFeedback').innerHTML = 'Your Akan name is' + genderFemale;
+              // alert("your Akan name is" + genderFemale);
+          }
+      } else {
+          alert("Invalid gender use F/M");
+      }
   }
-
-  else if( document.myForm.month.value == "" || isNaN( document.myForm.month.value ) ||
-
-  document.myForm.month.value.length != 2 || document.myForm.month.value > 12  || document.myForm.month.value <= 0){
-
-     alert( "Please provide your month of birth! between 1 and 12" );
-
-     document.myForm.month.focus() ;
-
-     return false;
-
+  else {
+      alert("wrong date format please use YYYY-MM-DD\nexample 2022-10-08");
   }
-
-  else if( document.myForm.date.value == "" || isNaN( document.myForm.month.value ) ||
-
-  document.myForm.month.value.length != 2|| document.myForm.date.value > 31 || document.myForm.date.value <= 0) {
-
-     alert( "Please provide a valid date that you were born in!" );
-
-     document.myForm.day.focus() ;
-
-     return false;
-
-  }
-
-  else if(genders[0].checked==false && genders[1].checked==false ) {
-
-      alert("You must select male or female");
-
-      return false;
-
-  }  
-
-  else{
-
-    return true ;
-
-  }
-
- 
-
-}
-
- 
-
-function calculateDayValue(){
-
-  year = document.getElementById("year").value;
-
-  CC = parseInt(year.substring(0,2));
-
-  YY = parseInt(year.substring(2,4));
-
-  MM = parseInt(document.getElementById("month").value);
-
-  DD = parseInt(document.getElementById("date").value);
-
-  d = ( ( (CC/4) -2*CC-1) + ( (5*YY/4) ) + ((26*(MM+1)/10) ) + DD)%7;
-
-  console.log(d);
-
-  return (Math.floor(d));
-
-}
-
- 
-
-function getGender(){
-
-  var genders = document.getElementsByName("gender");
-
-  if(genders[0].checked == true){
-
-    var gender = "male";
-
-  }
-
-  else if(genders[1].checked == true){
-
-    var gender = "female";
-
-  }
-
-  else{
-
-    return false;
-
-  }
-
-  switch(gender){
-
-    case "male":
-
-      if (dayValue == 1){
-
-        alert("You were born on " +dayWeekNames[0] + " and Your akan name is " +maleAkanNames[0]+"!");
-
-      }
-
-      else if(dayValue == 2){
-
-        alert("You were born on "+dayWeekNames[1] + " and Your akan name is " +maleAkanNames[1]+"!");
-
-      }
-
-      else if(dayValue == 3){
-
-        alert("You were born on " +dayWeekNames[2]+ " and Your akan name is " +maleAkanNames[2]+"!");
-
-      }
-
-      else if(dayValue == 4){
-
-        alert("You were born on "+dayWeekNames[3] +  " and Your akan name is " +maleAkanNames[3]+"!");
-
-      }
-
-      else if(dayValue == 5){
-
-        alert("You were born on "+dayWeekNames[4] +  " and Your akan name is " +maleAkanNames[4]+"!");
-
-      }
-
-      else if(dayValue == 6){
-
-        alert("You were born on "+dayWeekNames[5] +  " and Your akan name is " +maleAkanNames[5]+"!");
-
-      }
-
-      else if(dayValue == -0){
-
-        alert("You were born on "+dayWeekNames[6] + " and Your akan name is " +maleAkanNames[6]+"!");
-
-      }
-
-    break;
-
-    case "female":
-
-      if (dayValue == 1){
-
-        alert("You were born on "+dayWeekNames[0] + " and Your akan name is  " +femaleAkanNames[0]+"!");
-
-      }
-
-      else if(dayValue == 2){
-
-        alert("You were born on " +dayWeekNames[1] + " and Your akan name is " +femaleAkanNames[1]+"!");
-
-      }
-
-      else if(dayValue == 3){
-
-        alert("You were born on " +dayWeekNames[2] + " and Your akan name is " +femaleAkanNames[2]+"!");
-
-      }
-
-      else if(dayValue == 4){
-
-        alert("You were born on " +dayWeekNames[3] + " and Your akan name is " +femaleAkanNames[3]+"!");
-
-      }
-
-      else if(dayValue == 5){
-
-        alert("You were born on " +dayWeekNames[4] + " and Your akan name is " +femaleAkanNames[4]+"!");
-
-      }
-
-      else if(dayValue == 6){
-
-        alert("You were born on " +dayWeekNames[5] + " and Your akan name is " + femaleAkanNames[5]+"!");
-
-      }else if(dayValue == -0){
-
-        alert("You were born on " +dayWeekNames[6] + " and Your akan name is " +femaleAkanNames[6]+"!");
-
-      }
-
-    break
-
-    default:
-
-         
-
-  }
-
-}
-
-function findName(){
-
-  dayValue = calculateDayValue();
-
-  getGender();
-
 }
